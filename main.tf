fb192ddfa17ee636c29e687b317b99c98135c9aa
Dave Arnold <darnold@hashicorp.com>
2018-10-03 15:31:49 -0700

    initial add

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_compute_subnetwork" "new_subnetwork" {
+  name          = "${var.name}"
+  region        = "${var.subnetwork-region}"
+  network       = "${var.vpc}"
+  ip_cidr_range = "${var.ip_cidr_range}"
+  description   = "${var.description}"
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type        = "string"
+  description = "Subnet name to create in GCP"
+}
+
+variable "description" {
+  type        = "string"
+  description = "Description of subnet to create in GCP"
+  default     = "a subnet"
+}
+
+variable "vpc" {
+  type        = "string"
+  description = "Direct link to the network"
+}
+
+variable "subnetwork-region" {
+  type        = "string"
+  default     = ""
+  description = "Zone associated with the subnet. This defaults to the region configured in the provider."
+}
+
+variable "ip_cidr_range" {
+  type        = "string"
+  description = "IP range to book"
+}
